78f3aea73fe982e929139f0880b2d21afe705bac
PureDevr <julian.set2020@gmail.com>
2023-07-27 14:42:26 +0800

    Changed main, make the app continue to run until 0 exit code is inputted

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ from modules.app3_num_guesser import num_guesser_prompt
 import modules.app4_tictactoe
 
 if __name__ == '__main__':
+    while True:
+        while True:  # infinite loop until the break
+            # asks user the length of the password
+            listOfApps: list[str] = ["Password Generator-------1", "Object Sorter------------2",
+                                     "Number Guesser-----------3", "Tic Tac Toe--------------4",
+                                     "Exit App-----------------0"]
+            print("Select the application:")
+            for app in listOfApps:
+                print(app)
+            user_input = input("> ")
+            user_choice = ''.join(c for c in user_input if c.isnumeric())
+            if len(user_choice) > 0:
+                if 0 <= int(user_choice) <= len(listOfApps):
+                    break  # break after there's a numeric & legit length
 
-    while True:  # infinite loop until the break
-        # asks user the length of the password
-        listOfApps: list[str] = ["Password Generator-------1", "Object Sorter------------2",
-                                 "Number Guesser-----------3", "Tic Tac Toe--------------4"]
-        print(len(listOfApps))
-        print("Select the application:")
-        for app in listOfApps:
-            print(app)
-        user_input = input("> ")
-        user_choice = ''.join(c for c in user_input if c.isnumeric())
-        if len(user_choice) > 0:
-            if 0 < int(user_choice) <= len(listOfApps):
-                break  # break after there's a numeric & legit length
-
-    user_choice = int(user_choice)
-    match int(user_choice):
-        case 1:
-            password_gen_prompt()
-        case 2:
-            char_sorter_prompt()
-        case 3:
-            num_guesser_prompt()
-        case 4:
-            modules.app4_tictactoe.tictactoe_prompt()
+        user_choice = int(user_choice)
+        match int(user_choice):
+            case 0:
+                break
+            case 1:
+                password_gen_prompt()
+            case 2:
+                char_sorter_prompt()
+            case 3:
+                num_guesser_prompt()
+            case 4:
+                modules.app4_tictactoe.tictactoe_prompt()
